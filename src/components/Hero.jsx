4f68d62becd7079cfb73172a8dc10bedb1ae24df
Zaki abdullahi <qable2121@gmail.com>
2023-08-01 22:27:20 +0300

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,17 @@ const Hero = () => {
         src="https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=389&q=80"
         alt=""
       />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum alias
-        sapiente aliquam sed voluptatum incidunt minima atque eaque, temporibus,
-        porro dignissimos maxime distinctio nisi cupiditate, consectetur at
-        laboriosam eos iste.
-      </p>
+
+      <div className="right">
+        <h1>Welcome</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum alias
+          sapiente aliquam sed voluptatum incidunt minima atque eaque,
+          temporibus, porro dignissimos maxime distinctio nisi cupiditate,
+          consectetur at laboriosam eos iste.
+        </p>
+        <button className="btn">Learn more</button>
+      </div>
     </div>
   );
 };
